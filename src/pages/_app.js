bc307ec27318d955c1a018bd451210d7bc903bbe
whatacotton <whatacotton@gmail.com>
2024-01-10 20:51:30 +0900

    画像サイズの指定

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,12 @@
-
 import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
-    return <> <Head
-        rel="stylesheet"
-        href="https://unpkg.com/modern-css-reset/dist/reset.min.css"
-    />
+    return (<>
+        <Head
+            rel="stylesheet"
+            href="https://unpkg.com/modern-css-reset/dist/reset.min.css"
+        />
         <Component {...pageProps} />
-    </>
+    </>)
 
 }
 export default MyApp;
